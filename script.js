d8549d8e2fbe49994a384d27eb0663b0ee69cedd
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-15 19:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let volval=document.querySelector("#volume");
 function volume_sphere(event) {
 	event.preventDefault();
     let rad=radiusVal.value;
-	let vol=volval.value;
-	if(rad<0){
+	// let vol=volval.value;
+	if(rad<0||isNaN(rad)){
+		volval.value="NaN";
 		return;
 	}
-	let v=(4/3) * Math.PI * Math.pow(radius, 3);
+	let v=(4/3) * Math.PI * Math.pow(rad, 3);
 	v=v.toFixed(4);
-	vol.innertext=v;
+	volval.value=v;
 	
 	
   
