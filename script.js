b2f4168ef03200690f7847d9fc38ca076f53599e
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-15 19:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function volume_sphere() {
-    //Write your code here
+let radiusVal=document.querySelector("#radius");
+let volval=document.querySelector("#volume");
+function volume_sphere(event) {
+	event.preventDefault();
+    let rad=radiusVal.value;
+	let vol=volval.value;
+	if(rad<0){
+		return;
+	}
+	let v=(4/3) * Math.PI * Math.pow(radius, 3);
+	v=v.toFixed(4);
+	vol.innertext=v;
+	
+	
   
 } 
 
